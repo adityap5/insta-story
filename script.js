e6295b63c9a23d2d8a85237d60980ba6ffebda0a
adityap5 <aditya29pippal@gmail.com>
2023-09-18 20:18:29 +0530

    1

---
+++ script.js
@@ -0,0 +1,10 @@
+
+// var crsr = document.querySelector(".cursor")
+// var body = document.querySelector("body");
+
+// body.addEventListener("mousemove",function(dets){
+//   crsr.style.left = dets.x + "px";
+//   crsr.style.top = dets.y + "px";
+
+// })
+
